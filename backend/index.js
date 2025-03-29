6426fcfabef5b1c9eb84afec3c64c4a5be93ea66
illum1nadi <adityainduri@gmail.com>
2025-03-29 21:54:43 +0530

    Initialised node and basic boilerplate for backend

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.post("/todo", (req, res) => {
+
+})
+
+
+app.get("/todos", (req, res) => {
+  
+})
+
+
+app.put("/completed", (req, res) => {
+  
+})
+
+
