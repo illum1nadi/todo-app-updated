63a5580d9f9cb8f0757e722bbd883d4e7d9089e6
illum1nadi <adityainduri@gmail.com>
2025-03-29 23:11:15 +0530

    Created frontend components on react.

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
-import { createTodo, updateTodo } from "./types";
-import { todo } from "./db";
+const {createTodo, updateTodo} = require("./types");
+const {todo} = require("./db")
 
 const app = express();
 
@@ -30,12 +30,13 @@ app.post("/todo", async (req, res) => {
 
 app.get("/todos", async (req, res) => {
   const todos = await todo.find();
+  res.json(todos);
 })
 
 
 app.put("/completed", async (req, res) => {
   const updatePayload = req.body;
-  const parsedPayload = updateTodo(updatePayload);
+  const parsedPayload = updateTodo.safeParse(updatePayload);
   if(!parsedPayload.success) {
     res.status(411).json({
       msg : "The input format was incorrect"
@@ -54,4 +55,5 @@ app.put("/completed", async (req, res) => {
   })
 })
 
+app.listen(3002, console.log("Server is listening on port 3002."))
 
---
+++ frontend/src/components/Todos.jsx
@@ -0,0 +1,8 @@
+export function Todos() {
+  return <div>
+    <h1>Go to gym</h1> <br />
+    <h2>You need to go to gym</h2> <br />
+
+    <button>Complete</button>
+  </div>
+}
