88beed78cbc814307491e4e4c9302076a5ad04ac
illum1nadi <adityainduri@gmail.com>
2025-03-29 22:02:02 +0530

    Added Zod input validation

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,18 @@
 const express = require("express");
+import { createTodo, updateTodo } from "./types";
 const app = express();
 
 app.use(express.json());
 
 app.post("/todo", (req, res) => {
-
+  const createPayload = req.body;
+  const parsedPayload = createTodo.safeParse(createPayload);
+  if(!parsedPayload.success) {
+    res.status(411).json({
+      msg : "The input format was incorrect"
+    })
+    return;
+  }
 })
 
 
@@ -14,7 +22,14 @@ app.get("/todos", (req, res) => {
 
 
 app.put("/completed", (req, res) => {
-  
+  const updatePayload = req.body;
+  const parsedPayload = updateTodo(updatePayload);
+  if(!parsedPayload.success) {
+    res.status(411).json({
+      msg : "The input format was incorrect"
+    })
+    return;
+  }
 })
 
 
